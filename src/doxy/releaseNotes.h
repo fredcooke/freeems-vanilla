a4ed07ef107caf43b7e9afe354f9b27ffc9b6ff0
FredCooke <fred.cooke@gmail.com>
2008-12-31 21:29:44 +0000

    Migrated changelogs to doxygen html style instead of flat text file.

---
+++ src/doxy/releaseNotes.h
@@ -0,0 +1,141 @@
+/** @page releaseNotes Release Notes
+ *
+ * This page contains a list of all notable properties of this release. For
+ * old change logs click @ref oldReleaseNotes "here".
+ *
+ * @b New @b Features:
+ * - An awesome feature
+ * - Another awesome feature
+ *
+ * @b Differences:
+ * - This changed
+ * - This also changed
+ *
+ * @b Known @b Issues:
+ * - A nasty bug
+ * - Another nasty bug
+ *
+ * @anchor oldReleaseNotes
+ * @b Old @b Notes:
+ * - @subpage releaseNotes_0_0_X
+ * - @subpage releaseNotes_0_1_X
+ */
+
+/** @page releaseNotes_0_0_X Release Notes - 0.0.X Versions
+ *
+ * - @subpage releaseNotes_0_0_1
+ * - @subpage releaseNotes_0_0_2
+ * - @subpage releaseNotes_0_0_3
+ * - @subpage releaseNotes_0_0_4
+ * - @subpage releaseNotes_0_0_5
+ * - @subpage releaseNotes_0_0_6
+ * - @subpage releaseNotes_0_0_7
+ * - @subpage releaseNotes_0_0_8
+ * - @subpage releaseNotes_0_0_9
+ * - @subpage releaseNotes_0_0_10
+ * - @subpage releaseNotes_0_0_11
+ * - @subpage releaseNotes_0_0_12
+ * - @subpage releaseNotes_0_0_13
+ * - @subpage releaseNotes_0_0_14
+ * - @subpage releaseNotes_0_0_15
+ * - @subpage releaseNotes_0_0_16
+ * - @subpage releaseNotes_0_0_17
+ * - @subpage releaseNotes_0_0_18
+ */
+
+/** @page releaseNotes_0_1_X Release Notes - 0.1.X Versions
+ *
+ * It is wishful thinking putting this here at this stage, but never the less, here it is :-)
+ *
+ * Hopefully we can put it to good use fairly soon!
+ */
+
+/** @page releaseNotes_0_0_1 FreeEMS 0.0.1
+ *
+ * Initial version
+ */
+
+/** @page releaseNotes_0_0_2 FreeEMS 0.0.2
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_3 FreeEMS 0.0.3
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_4 FreeEMS 0.0.4
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_5 FreeEMS 0.0.5
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_6 FreeEMS 0.0.6
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_7 FreeEMS 0.0.7
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_8 FreeEMS 0.0.8 Glen
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_9 FreeEMS 0.0.9 Exponent
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_10 FreeEMS 0.0.10 Squashed
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_11 FreeEMS 0.0.11 Cookin'
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_12 FreeEMS 0.0.12 Bastard
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_13 FreeEMS 0.0.13 GooGooGaGa
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_14 FreeEMS 0.0.14 Entropy
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_15 FreeEMS 0.0.15 Avogadro
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_16 FreeEMS 0.0.16 FlashGordon
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_17 FreeEMS 0.0.17 SpudEchoes
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_18 FreeEMS 0.0.18 JackTheRipper
+ *
+ * stuff
+ */
