ce5e5d69ec2e3303d4a4ff814990c7b5f5549baa
FredCooke <fred.cooke@gmail.com>
2009-01-01 00:30:55 +0000

    Add main page in with decent content, formatting and layout etc. Add general strategies page document.

---
+++ src/doxy/strategies.h
@@ -0,0 +1,16 @@
+/** @page generalStrategies General Strategies
+ *
+ * This page exists to document general coding strategies used in this
+ * firmware. Each item should contain a title, description, situations
+ * it is useful in and somewhere you can see an example of it in use.
+ *
+ * Reading/Writing from/to a specific page :
+ *
+ * Save the current page value to a variable, change the page value to the desired one, read or write the paged memory, change the page back and return the value
+ *
+ * This is applicable to all three available memory types: Flash, RAM and EEPROM.
+ *
+ * Examples of its use can be found in flashWrite.c
+ *
+ * @todo TODO TBC (other strategies)
+ */
