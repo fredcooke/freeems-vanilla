cad412c21628b51212abdcd925c7693f4cf07fae
FredCooke <fred.cooke@gmail.com>
2009-01-03 16:45:19 +0000

    Fixed spacing, indentation, blank lines, added a todo tag and reworded one param tag a little.

--- src/flashWrite.c
+++ src/flashWrite.c
@@ -31,23 +31,22 @@
 #include <string.h>
 
 
-/**
-*  Erases a sector of flash memory
-*
-*  This will erase a 1k sector in flash.  Write 0xFFFF to the starting sector to be
-*  erased, 0xFFFF will be written regardless. Register the flash sector erase command(0x40)
-*  and call StackBurner();.  If you try to erase a protected sector you will get PVIOL in
-*  the FSTAT register.
-* 
-* @author Sean Keys
-*
-* @warning This will erase an entire 1k block starting at the address you pass
-*
-* @param PPage the flashPage you are referring to
-* @param flashAddr the first address in the sector
-*
-* @return an error code. Zero means success, anything else is a failure.
-*/
+/** Erases a sector of flash memory
+ *
+ * This will erase a 1k sector in flash.  Write 0xFFFF to the starting sector
+ * to be erased, 0xFFFF will be written regardless. Register the flash sector
+ * erase command(0x40) and call StackBurner();. If you try to erase a protected
+ * sector you will get PVIOL in the FSTAT register.
+ *
+ * @author Sean Keys
+ *
+ * @warning This will erase an entire 1k block starting at flashAddr
+ *
+ * @param PPage the flash page the sector is in
+ * @param flashAddr the start address of the sector
+ *
+ * @return An error code. Zero means success, anything else is a failure.
+ */
 unsigned short eraseSector(unsigned char PPage, unsigned short *flashAddr){
 
 	if (((unsigned short)flashAddr % flashSectorSize) != 0){
@@ -64,6 +63,8 @@ unsigned short eraseSector(unsigned char PPage, unsigned short *flashAddr){
 
 	return 0;
 }
+
+
 /**
  * Writes a block of memory to flash.
  *
@@ -169,35 +170,37 @@ unsigned short writeBlock(blockDetails* details, void* buffer){
 	}
 	return 0;
 }
-/**
-*  Writes a sector from memory to a sector in flash
-*
-*  Uses writeWord to write a 1k block from sourceAddress(RAM) to
-*  flashDestinationAddress, one word at a time. Give it the starting memory address and the destination
-*  flash address.  Both addresses will be incremented by 1 word after a successful writeWord,
-*  until the whole 1024 byte sector has been written.  Before any writing occurs
-*  eraseSector is called to make sure the destination is blank.
-*
-* @author Sean Keys
-*
-* @param RPage the page of RAM the RAMSourceAddress is located
-* @param RAMSourceAddress the address of the source data
-* @param PPage the page of flash where your flashDestinationAddress is located
-* @param flashDestinationAddress where your data will be written to in flash
-*
-* @return an error code. Zero means success, anything else is a failure.
-*/
+
+
+/** Writes a sector from memory to a sector in flash
+ *
+ * Uses writeWord to write a 1k block from sourceAddress(RAM) to
+ * flashDestinationAddress, one word at a time. Give it the starting memory
+ * address and the destination flash address. Both addresses will be
+ * incremented by 1 word after a successful writeWord, until the whole 1024
+ * byte sector has been written.  Before any writing occurs eraseSector is
+ * called to make sure the destination is blank.
+ *
+ * @author Sean Keys
+ *
+ * @param RPage the page of RAM the RAMSourceAddress is located
+ * @param RAMSourceAddress the address of the source data
+ * @param PPage the page of flash where your flashDestinationAddress is located
+ * @param flashDestinationAddress where your data will be written to in flash
+ *
+ * @return an error code. Zero means success, anything else is a failure.
+ */
 unsigned short writeSector(unsigned char RPage, unsigned short* RAMSourceAddress, unsigned char PPage , unsigned short* flashDestinationAddress){
 
-	if (((unsigned short)flashDestinationAddress % flashSectorSize) != 0){
+	if(((unsigned short)flashDestinationAddress % flashSectorSize) != 0){
 			return addressNotSectorAligned;
-		}
+	}
 
 	if(((unsigned short)flashDestinationAddress) < 0x4000){
 		return addressNotFlashRegion;
 	}
 
-	//TODO Decide if we need to disable interrupts since we are manually setting Flash/RAM pages.
+	/// @todo TODO Decide if we need to disable interrupts since we are manually setting Flash/RAM pages.
 	eraseSector((unsigned char)PPage, (unsigned short*)flashDestinationAddress);  /* First Erase our destination block */
 
 	unsigned short wordCount = flashSectorSizeInWords;
@@ -227,24 +230,27 @@ unsigned short writeSector(unsigned char RPage, unsigned short* RAMSourceAddress
 	PPAGE = currentPPage;
 	return 0;
 }
+
+
 /**	Program Command
-*  This will write 1 word to an empty(0xFFFF) flash address.  If you try to write to an
-*  address containing data(not 0xFFFF),an error will register at FSTAT.  The embedded
-*  algorithm works like this, just write to the desired flash  address as you would any
-*  other writable address. Then register the program command(0x20)
-*  at FCDM, the rest is handled by StackBurner();
-*
-* @author Sean Keys
-*
-* @warning Be sure your destination address is not protected or you will flag an error in FSTAT
-*
-* @param flashDestination where you want to write your data
-* @param data the data you are going to write
-*
-* @return an error code. Zero means success, anything else is a failure.
-*/
+ *
+ * This will write 1 word to an empty(0xFFFF) flash address. If you try to
+ * write to an address containing data(not 0xFFFF),an error will register at
+ * FSTAT. The embedded algorithm works like this, just write to the desired
+ * flash address as you would any other writable address. Then register the
+ * program command(0x20) at FCDM, the rest is handled by StackBurner();
+ *
+ * @author Sean Keys
+ *
+ * @warning Be sure your destination address is not protected or you will flag an error in FSTAT
+ *
+ * @param flashDestination where you want to write your data
+ * @param data the data you are going to write
+ *
+ * @return an error code. Zero means success, anything else is a failure.
+ */
 unsigned short writeWord(unsigned short* flashDestination, unsigned short data){
-	if ((unsigned short)flashDestination & 0x0001){
+	if((unsigned short)flashDestination & 0x0001){
 		return addressNotWordAligned;
 	}
 
