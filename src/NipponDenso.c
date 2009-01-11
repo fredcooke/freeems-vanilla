a7449bece0aeea7568a4bfb253f77ddd4f5d060b
FredCooke <fred.cooke@gmail.com>
2009-01-11 22:07:23 +0000

    Fixed doxy tag.

--- src/NipponDenso.c
+++ src/NipponDenso.c
@@ -23,7 +23,7 @@
 	Thank you for choosing FreeEMS to run your engine! */
 
 
-/**	@file NipponDensoISR.c
+/**	@file NipponDenso.c
  * @ingroup interruptHandlers
  * @ingroup enginePositionRPMDecoders
  *
