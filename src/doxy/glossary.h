cdabe5ac03c35400a5ef1c6453b80fc0e850f329
FredCooke <fred.cooke@gmail.com>
2009-09-03 12:41:27 -0700

    Add unfinished glossary document to doxy stuff.

---
+++ src/doxy/glossary.h
@@ -0,0 +1,72 @@
+/** @page glossary Glossary
+ *
+ * The field of engine management has many technical terms and acronyms. This
+ * page exists to help make sense of them for the uninitiated who want to come
+ * up to speed as quickly as possible.
+ *
+ * @b Acronyms and Abbreviations:
+ * - EMS - Engine Management System
+ * - EFI - Electronic Fuel Injection
+ * - ECT - Ehanced Capture Timer
+ * - PIT - Precision Interrupt Timer??
+ * - GNU - GNU's Not Unix
+ * - GCC - GNU C Compiler
+ * - GPL - GNU Public License
+ * - RAM - Random Access Memory
+ * - ROM - Read Only Memory
+ * - Asm - Assembly Code
+ * - SCI - Serial Communication Interface
+ * - SPI - Serial Peripheral Interface
+ * - TX - Transmit/Transmission
+ * - RX - Receive/Reception
+ * - ISR - Interrupt Service Routine
+ * - COP - Computer Operating Properly
+ * - COP - Coil On Plug
+ * - CNP - Coil Near Plug
+ * - OO - Object Oriented
+ * - RPM - Revolutions Per Minute
+ * - MAP - Manifold Absolute Pressure
+ * - IAP - Intercooler Absolute Pressure
+ * - AAP - Absolute Atmospheric Pressure
+ * - MAF - Mass Air Flow
+ * - ATD - Analog To Digital
+ * - ADC - Analog to Digital Converter
+ * - PW - Pulse Width
+ * - PWM - Pulse Width Modulation
+ * - BPW - Base Pulse Width
+ * - ECO - Exhaust Gas Oxygen
+ * - WBO2 - Wide Band Oxygen
+ * - RTC - Real Time Clock
+ * - RTI - Real Time Interrupt
+ * - BDM - Background Debug Module
+ * - CAN - Controller Area Network
+ * - IAT - Intake Air Temperature
+ * - MAT - Manifold Air Temperature
+ * - CLT - CooLant Temperature
+ * - CHT - Coolant/Head Temperature
+ * - ERT - Engine Reference Temperature
+ * - ETE - Engine Temperature Enrichment
+ * - VE - Volumetric Efficiency
+ * - TPS - Throttle Position Sensor
+ * - BRV - Battery Reference Voltage
+ * - AFR - Air Fuel Ratio
+ * - IDT - Injector Dead Time
+ * - TFC - Transient Fuel Correction
+ * -
+ *
+ * @b Terminology
+ * - Function - A block of code with aname, optional input and optional output.
+ * - Method - An alternative OO name for a function.
+ * - Void - Indicates that no input or output is required or will be given.
+ * - Stack - An area of memory where code is stored that grows as it is run.
+ * - Dwell - The time current is applied to a coil terminated by the spark event.
+ * - Lambda - Ratio of air fuel ratio that is independent of fuel type.
+ * - Stoichiometric - The ratio of fuel to air with none of either left over after combustion.
+ * - Bit Bang - To turn a pin on and off manually from the code, not hardware.
+ * - Polling - To read the state of a pin manually from the code, not hardware.
+ */
+
+/**	@file glossary.h
+ * @ingroup doxygenDocFiles
+ * @brief Glossary and acronym explanations
+ */
