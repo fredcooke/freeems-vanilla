1819d7d0dc5a2a5b01ec80795f7b6d3b8c3ab2f0
Fred Cooke <fred.cooke@gmail.com>
2010-05-23 20:48:58 +1200

    Draft release notes added, more rearrangement of blocks of doxy for minimal and cleaner diffs.

--- src/doxy/releaseNotes.h
+++ src/doxy/releaseNotes.h
@@ -3,6 +3,38 @@
  * This page contains a list of all notable properties of this release. For
  * old change logs click @ref oldReleaseNotes "here".
  *
+ *
+ * @b General @b Notes
+ *
+ * A version of the code very similar to 0.1.0 was the first version of FreeEMS to run an engine!
+ *
+ * The version that I used on my Volvo 740GL wagon sporting a B230F engine was a little less functional
+ * and more hacked together. What you see here represents the same code tidied up and with some minor
+ * bug fixes and documentation changes.
+ *
+ * The version of the tuner used to talk to and tune the very first FreeEMS prototype can be found with
+ * the following git commit hash 5188317b6e7418238686602d3b787250398f1ad4 or more conveniently at the
+ * following link http://github.com/fredcooke/freeems-tuner/commit/5188317b6e7418238686602d3b787250398f1ad4
+ *
+ * To get the exact code used on the volvo, take any hash between 97f5ad1f0e6b2ef30bd987c3becc764a5d658bf5
+ * and 0cc18370652f096cbcca2226e779a0370a77b113, go to line 110 or 110 and change TRUE to FALSE and build.
+ * What you have if you do the above is exactly what I used on my Volvo sans the tune itself. If you want
+ * the tune too, the S19 is included in the firmware directory. Here is a link to an appropriate version
+ * of the code : http://github.com/fredcooke/freeems-vanilla/commit/0cc18370652f096cbcca2226e779a0370a77b113
+ *
+ *
+ * @b New @b Features:
+ * - ?
+ *
+ * @b Differences:
+ * - ?
+ *
+ * @b Known @b Issues:
+ * - ?
+ */
+
+/** @page releaseNotes_0_0_19 FreeEMS 0.0.19 Doxygenated
+ *
  * @b New @b Features:
  * - Almost all the code is doxygen commented
  * - The Makefile has been majorly reworked to allow for multiple output files
@@ -12,8 +44,100 @@
  *
  * @b Known @b Issues:
  * - The windows makefile is out of date and does not work
+ */
+
+/** @page releaseNotes_0_0_18 FreeEMS 0.0.18 JackTheRipper
  *
- * @anchor oldReleaseNotes
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_17 FreeEMS 0.0.17 SpudEchoes
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_16 FreeEMS 0.0.16 FlashGordon
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_15 FreeEMS 0.0.15 Avogadro
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_14 FreeEMS 0.0.14 Entropy
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_13 FreeEMS 0.0.13 GooGooGaGa
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_12 FreeEMS 0.0.12 Bastard
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_11 FreeEMS 0.0.11 Cookin'
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_10 FreeEMS 0.0.10 Squashed
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_9 FreeEMS 0.0.9 Exponent
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_8 FreeEMS 0.0.8 Glen
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_7 FreeEMS 0.0.7
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_6 FreeEMS 0.0.6
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_5 FreeEMS 0.0.5
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_4 FreeEMS 0.0.4
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_3 FreeEMS 0.0.3
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_2 FreeEMS 0.0.2
+ *
+ * stuff
+ */
+
+/** @page releaseNotes_0_0_1 FreeEMS 0.0.1
+ *
+ * Initial version
+ */
+
+/* @page oldReleaseNotes Old Release Notes
+ *
  * @b Old @b Notes:
  * - @subpage releaseNotes_0_0_X
  * - @subpage releaseNotes_0_1_X
@@ -39,6 +163,7 @@
  * - @subpage releaseNotes_0_0_16
  * - @subpage releaseNotes_0_0_17
  * - @subpage releaseNotes_0_0_18
+ * - @subpage releaseNotes_0_0_19
  */
 
 /** @page releaseNotes_0_1_X Release Notes - 0.1.X Versions
@@ -46,96 +171,6 @@
  * It is wishful thinking putting this here at this stage, but never the less, here it is :-)
  *
  * Hopefully we can put it to good use fairly soon!
- */
-
-/** @page releaseNotes_0_0_1 FreeEMS 0.0.1
- *
- * Initial version
- */
-
-/** @page releaseNotes_0_0_2 FreeEMS 0.0.2
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_3 FreeEMS 0.0.3
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_4 FreeEMS 0.0.4
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_5 FreeEMS 0.0.5
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_6 FreeEMS 0.0.6
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_7 FreeEMS 0.0.7
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_8 FreeEMS 0.0.8 Glen
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_9 FreeEMS 0.0.9 Exponent
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_10 FreeEMS 0.0.10 Squashed
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_11 FreeEMS 0.0.11 Cookin'
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_12 FreeEMS 0.0.12 Bastard
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_13 FreeEMS 0.0.13 GooGooGaGa
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_14 FreeEMS 0.0.14 Entropy
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_15 FreeEMS 0.0.15 Avogadro
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_16 FreeEMS 0.0.16 FlashGordon
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_17 FreeEMS 0.0.17 SpudEchoes
- *
- * stuff
- */
-
-/** @page releaseNotes_0_0_18 FreeEMS 0.0.18 JackTheRipper
- *
- * stuff
  */
 
 /**	@file releaseNotes.h
