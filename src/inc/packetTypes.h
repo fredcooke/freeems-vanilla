6991a9e69dea10004ea71aacaaeeaf2d0278cd18
Fred Cooke <fred.cooke@gmail.com>
2010-12-15 01:43:06 +1300

    Whitespace change ONLY.

--- src/inc/packetTypes.h
+++ src/inc/packetTypes.h
@@ -47,55 +47,55 @@
 /*&&&&&&&&&&&&&&&&&&&&&&&&  Protocol Payload Type IDs  &&&&&&&&&&&&&&&&&&&&&&&*/
 
 /* Firmware Independent functions */
-#define requestInterfaceVersion		  0
-#define requestFirmwareVersion		  2
-#define requestMaxPacketSize		  4
-#define requestEchoPacketReturn		  6
-#define requestSoftSystemReset		  8	/* System comes up fresh, so no response */
-//efine replytoSoftSystemReset		  9	/* This is reserved */
-#define requestHardSystemReset		 10	/* System comes up fresh, so no response */
-//efine replytoHardSystemReset		 11	/* This is reserved */
+#define requestInterfaceVersion       0
+#define requestFirmwareVersion        2
+#define requestMaxPacketSize          4
+#define requestEchoPacketReturn       6
+#define requestSoftSystemReset        8	/* System comes up fresh, so no response */
+//efine replytoSoftSystemReset        9	/* This is reserved */
+#define requestHardSystemReset       10	/* System comes up fresh, so no response */
+//efine replytoHardSystemReset       11	/* This is reserved */
 
 
-//efine requestAsyncErrorCode		 12	/* This is reserved */
-#define asyncErrorCodePacket		 13	/* NOTE : Unrequested error event code packet */
-//efine requestAsyncDebugInfo		 14	/* This is reserved */
-#define asyncDebugInfoPacket		 15	/* NOTE : Unrequested debug information packet */
+//efine requestAsyncErrorCode        12	/* This is reserved */
+#define asyncErrorCodePacket         13	/* NOTE : Unrequested error event code packet */
+//efine requestAsyncDebugInfo        14	/* This is reserved */
+#define asyncDebugInfoPacket         15	/* NOTE : Unrequested debug information packet */
 
 
 /*&&&&&&&&&&&&&&&&&&&&&&&&  Firmware Payload Type IDs  &&&&&&&&&&&&&&&&&&&&&&&*/
 
 /* Whole block manipulation */
-#define replaceBlockInRAM			256
-#define replaceBlockInFlash			258
-#define retrieveBlockFromRAM		260
-#define retrieveBlockFromFlash		262
-#define burnBlockFromRamToFlash 	264
-#define eraseAllBlocksFromFlash 	266
-#define burnAllBlocksOfFlash 		268
+#define replaceBlockInRAM           256
+#define replaceBlockInFlash         258
+#define retrieveBlockFromRAM        260
+#define retrieveBlockFromFlash      262
+#define burnBlockFromRamToFlash     264
+#define eraseAllBlocksFromFlash     266
+#define burnAllBlocksOfFlash        268
 
 /* Main table manipulation */
-#define adjustMainTableCell			300
-#define adjustMainTableRPMAxis		302
-#define	adjustMainTableLoadAxis		304
+#define adjustMainTableCell         300
+#define adjustMainTableRPMAxis      302
+#define	adjustMainTableLoadAxis     304
 
 /* 2d table manipulation */
-#define adjust2dTableAxis			306
-#define adjust2dTableCell			308
+#define adjust2dTableAxis           306
+#define adjust2dTableCell           308
 
 /* Datalog request packets */
-#define requestBasicDatalog			400
-#define responseBasicDatalog		401 /* Defined because it can be used both synchronously and asynchronously */
-#define requestConfigurableDatalog	402
-#define responseConfigurableDatalog	403 /* Defined because it can be used both synchronously and asynchronously */
-#define setAsyncDatalogType			404
+#define requestBasicDatalog         400
+#define responseBasicDatalog        401 /* Defined because it can be used both synchronously and asynchronously */
+#define requestConfigurableDatalog  402
+#define responseConfigurableDatalog 403 /* Defined because it can be used both synchronously and asynchronously */
+#define setAsyncDatalogType         404
 
 /* Special function */
-#define forwardPacketOverCAN		500
-#define forwardPacketOverOtherUART	502
+#define forwardPacketOverCAN        500
+#define forwardPacketOverOtherUART  502
 
 /* Generic memory grabber for debugging */
-#define retrieveArbitraryMemory		600
+#define retrieveArbitraryMemory     600
 
 
 #else
