aea1a15cef6270fa61bd7d7e4c8d10429029bbc1
Fred Cooke <fred.cooke@gmail.com>
2011-02-15 12:05:46 +1300

    Make data use includes so that all are the same and less maintenance. Only temp before xml gen begins, but worth fixing now.

---
+++ src/data/tables/axis/mainTable-Load.h
@@ -0,0 +1,45 @@
+/* FreeEMS - the open source engine management system
+ *
+ * Copyright 2011 Fred Cooke
+ *
+ * This file is part of the FreeEMS project.
+ *
+ * FreeEMS software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FreeEMS software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with any FreeEMS software.  If not, see http://www.gnu.org/licenses/
+ *
+ * We ask that if you make any changes to this file you email them upstream to
+ * us at admin(at)diyefi(dot)org or, even better, fork the code on github.com!
+ *
+ * Thank you for choosing FreeEMS to run your engine!
+ */
+
+
+/**	@file mainTable-Load.h
+ *
+ * @ingroup dataInitialisers
+ *
+ * @brief A default set of loads for a boosted engine.
+ *
+ * This file only contains the data to be hash included into all mainTable Load sections
+ *
+ * @author Fred Cooke
+ */
+
+
+/* 21 load values starting at 16kPa and ending at 336kPa, only the first 19 are used by default */
+ 1600,	 3200,	 4800,	 6400,
+ 8000,	 9600,	11200,	12800,
+14400,	16000,	17600,	19200,
+20800,	22400,	24000,	25600,
+27200,	28800,	30400,	32000,
+33600
---
+++ src/data/tables/axis/mainTable-RPM.h
@@ -0,0 +1,46 @@
+/* FreeEMS - the open source engine management system
+ *
+ * Copyright 2011 Fred Cooke
+ *
+ * This file is part of the FreeEMS project.
+ *
+ * FreeEMS software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * FreeEMS software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with any FreeEMS software.  If not, see http://www.gnu.org/licenses/
+ *
+ * We ask that if you make any changes to this file you email them upstream to
+ * us at admin(at)diyefi(dot)org or, even better, fork the code on github.com!
+ *
+ * Thank you for choosing FreeEMS to run your engine!
+ */
+
+
+/**	@file mainTable-RPM.h
+ *
+ * @ingroup dataInitialisers
+ *
+ * @brief A default set of RPMs for a typical jap engine.
+ *
+ * This file only contains the data to be hash included into all mainTable RPM sections
+ *
+ * @author Fred Cooke
+ */
+
+
+/* 27 RPM values starting at 200 and ending at 9100, only the first 24 are used by default */
+   0,	 400,	1400,	2100,
+2800,	3500,	4200,	4900,
+5600,	6300,	7000,	7700,
+8400,	9100,	9800,  10500,
+11200,  11900,  12600,  13300,
+14000,  14700,  14400,  16100,
+16800,  17500,  18200
