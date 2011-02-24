28c0bd91ce785f8ca6786536465a763ddb24706a
Fred Cooke <fred.cooke@gmail.com>
2011-02-24 16:43:58 +1300

    Remove low level serial debug, no errors there, add syncFoundOn var instead.

--- src/main.c
+++ src/main.c
@@ -123,7 +123,8 @@ int  main(){ // TODO maybe move this to paged flash ?
 			/* UART/serial specific counters */
 			DerivedVars->sp1 = Counters.serialEscapePairMismatches + Counters.serialStartsInsideAPacket + Counters.serialPacketsOverLength + Counters.serialNoiseErrors + Counters.serialOverrunErrors + Counters.serialFramingErrors + Counters.serialParityErrors;
 			/* Generic com counters */
-			DerivedVars->sp2 = Counters.commsChecksumMismatches + Counters.commsPacketsUnderMinLength + Counters.commsDebugMessagesNotSent + Counters.commsErrorMessagesNotSent;
+//			DerivedVars->sp2 = Counters.commsChecksumMismatches + Counters.commsPacketsUnderMinLength + Counters.commsDebugMessagesNotSent + Counters.commsErrorMessagesNotSent;
+			DerivedVars->sp2 = syncCaughtOnThisEvent;
 			DerivedVars->sp3++;
 			DerivedVars->sp4 = (decoderFlags << 8) + coreStatusA;
 			DerivedVars->sp5 = currentEvent;
