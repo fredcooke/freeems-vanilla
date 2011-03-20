3292f5734852f673545f946e25458aa9ad28d255
Fred Cooke <fred.cooke@gmail.com>
2011-03-21 02:49:54 +1300

    Update to be closer for engine offset in stock dizzy position of running silvertop.

--- src/fuelAndIgnitionCalcs.c
+++ src/fuelAndIgnitionCalcs.c
@@ -191,7 +191,7 @@ void calculateFuelAndIgnition(){
 // add this to code degrees to find 0/TDC for cyl/output 1 or subtract from real degrees to get code degrees
 #define Mitsi4and1OffsetOnTruck  90
 #define HyundaiHackOffset        17
-#define SilverTop4age             0
+#define SilverTop4age           670
 
 	unsigned short decoderEngineOffset = HyundaiHackOffset;
 	if(decoderEngineOffset >= totalEventAngleRange){
