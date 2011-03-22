26f373de0559e9cb2cd85529b791ecacef083dae
Fred Cooke <fred.cooke@gmail.com>
2011-03-22 18:07:19 +1300

    Add script to test builds, johntramp, wanna have a crack at improving it? :-)

---
+++ bin/testclibuilds.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+CLIFLAGS="-D XGATE=1" make clean  s19
+sleep 1
+CLIFLAGS="-D TRUCK=1" make clean  s19
+sleep 1
+CLIFLAGS="-D HOTEL=1" make clean  s19
+sleep 1
+CLIFLAGS="-D PRESTO=1" make clean  s19
+sleep 1
+CLIFLAGS="-D SEANKR1=1" make clean  s19
+sleep 1
+CLIFLAGS="-D SEANKLT1=1" make clean  s19
+
+# Someone with elite bash skills grab the result from each of these and print them at the end, please.
