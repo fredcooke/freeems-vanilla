2cf52e04fc5c29b754fd544c5064594fd4872cca
Fred Cooke <fred.cooke@gmail.com>
2011-04-18 18:03:36 -0700

    Lift limits for annoyingly silent return in fuel/ign calcs that was stopping us from seeing output on ign channels with more than 6 configured.

--- src/fuelAndIgnitionCalcs.c
+++ src/fuelAndIgnitionCalcs.c
@@ -335,10 +335,13 @@ masterPulseWidth = refPW / numberOfInjectionsPerEngineCycle; // div by number of
 	if(decoderEngineOffset >= totalEventAngleRange){
 		return; /// @todo don't bother doing anything, settings don't make sense... TODO move this to init time to prevent bad config
 	}
-	if(numberOfIgnitionEvents > 6){
+	if(numberOfIgnitionEvents > 12){
 		return; /// @todo don't bother doing anything, settings don't make sense... TODO move this to init time to prevent bad config
 	}
-	if((cliConfigredNumberOfIgnitionEvents + numberOfInjectionEvents) > 6){
+	if(numberOfInjectionEvents > 12){
+		return; /// @todo don't bother doing anything, settings don't make sense... TODO move this to init time to prevent bad config
+	}
+	if((cliConfigredNumberOfIgnitionEvents + numberOfInjectionEvents) > 24){
 		return; /// @todo TODO temp, fix up
 	}
 /// @todo TODO create this check:
