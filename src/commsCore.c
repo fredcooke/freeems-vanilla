24a637ddee646f1aff5eeb5e375ce4d4c60ec9ba
Fred Cooke <fred.cooke@gmail.com>
2011-04-18 20:16:07 -0700

    Add ability to do custom logging without messing with major important files.

--- src/commsCore.c
+++ src/commsCore.c
@@ -53,6 +53,7 @@
 #include "inc/commsCore.h"
 #include "inc/init.h"
 #include <string.h> /// @todo TODO this is pulling in the system string.h not the m68hc1x version, and functions other than memcpy do not work because they are not in crt1.o or other included-by-default libs
+#include <customDatalogPopulator.c>
 
 
 /** @brief Populate a basic datalog packet
@@ -70,46 +71,8 @@ void populateBasicDatalog(){
 
 	/// @todo TODO setup proper sequence and clock with some sort of differential measurement log to log. insert in front of actual data because these are part of the log itself.
 
-	// temp debug, done here to ensure it matches the other data.
-//	CoreVars->DRPM = Counters.; currently in use as at wed nzst 3:10pm 16 march 2011
-//	CoreVars->DDRPM = Counters.testCounter1;
-//	CoreVars->DTPS = Counters.testCounter2;
-//	CoreVars->DMAP = Counters.testCounter3;
-	CoreVars->DDRPM = injectorMainPulseWidthsMath[1];
-	CoreVars->DTPS = postReferenceEventDelays[1];
-	CoreVars->DMAP = outputEventInputEventNumbers[1];
-
-	// Use these any way you like:
-	DerivedVars->zsp1 = Clocks.realTimeClockMain;
-	DerivedVars->zsp2 = Clocks.realTimeClockMillis;
-	DerivedVars->zsp3++;
-	DerivedVars->zsp4 = (decoderFlags << 8) + coreStatusA;
-	DerivedVars->zsp5 = Counters.calculationsPerformed;
-//	DerivedVars->zsp6 = Counters.testCounter0;
-	DerivedVars->zsp6 = injectorMainPulseWidthsMath[0];
-	DerivedVars->zsp7 = postReferenceEventDelays[0];
-	DerivedVars->zsp8 = outputEventInputEventNumbers[0];
-	DerivedVars->zsp9 = currentEvent;
-	DerivedVars->zsp10 = syncLostOnThisEvent;
-	DerivedVars->zsp11 = syncCaughtOnThisEvent;
-	DerivedVars->zsp12 = Counters.primaryTeethSeen;
-	DerivedVars->zsp13 = Counters.secondaryTeethSeen;
-	DerivedVars->zsp14 = *ticksPerDegree;
-//	DerivedVars->zsp15 = Counters.testCounter4;
-//	DerivedVars->zsp16 = Counters.testCounter5;
-//	DerivedVars->zsp17 = Counters.testCounter6;
-	DerivedVars->zsp15 = Counters.camSyncLosses;
-	DerivedVars->zsp16 = Counters.crankSyncLosses;
-	DerivedVars->zsp17 = Counters.combustionSyncLosses;
-	DerivedVars->zsp18 = Counters.camSyncCorrections;
-	DerivedVars->zsp19 = syncLostWithThisID;
-//	DerivedVars->zsp18 = Counters.DwellStretchedToSchedule;
-//	DerivedVars->zsp19 = Counters.TooFarToSchedule;
-
-	/* UART/serial specific counters */
-//	DerivedVars->zsp? = Counters.serialEscapePairMismatches + Counters.serialStartsInsideAPacket + Counters.serialPacketsOverLength + Counters.serialNoiseErrors + Counters.serialOverrunErrors + Counters.serialFramingErrors + Counters.serialParityErrors;
-	/* Generic com counters */
-//	DerivedVars->zsp? = Counters.commsChecksumMismatches + Counters.commsPacketsUnderMinLength + Counters.commsDebugMessagesNotSent + Counters.commsErrorMessagesNotSent;
+	// By default, default values are populated, but if you drop code into the custom directory, that replaces the defaults.
+	populateCustomDatalog();
 
 	/* Get core vars */
 	memcpy(TXBufferCurrentPositionHandler, CoreVars, sizeof(CoreVar));
---
+++ src/default/customDatalogPopulator.c
@@ -0,0 +1,44 @@
+// See README.txt in custom dir and this dir
+
+void populateCustomDatalog(){
+//	CoreVars->DRPM = Counters.; currently in use as at wed nzst 3:10pm 16 march 2011
+//	CoreVars->DDRPM = Counters.testCounter1;
+//	CoreVars->DTPS = Counters.testCounter2;
+//	CoreVars->DMAP = Counters.testCounter3;
+	CoreVars->DDRPM = injectorMainPulseWidthsMath[1];
+	CoreVars->DTPS = postReferenceEventDelays[1];
+	CoreVars->DMAP = outputEventInputEventNumbers[1];
+
+	// Use these any way you like:
+	DerivedVars->zsp1 = Clocks.realTimeClockMain;
+	DerivedVars->zsp2 = Clocks.realTimeClockMillis;
+	DerivedVars->zsp3++;
+	DerivedVars->zsp4 = (decoderFlags << 8) + coreStatusA;
+	DerivedVars->zsp5 = Counters.calculationsPerformed;
+//	DerivedVars->zsp6 = Counters.testCounter0;
+	DerivedVars->zsp6 = injectorMainPulseWidthsMath[0];
+	DerivedVars->zsp7 = postReferenceEventDelays[0];
+	DerivedVars->zsp8 = outputEventInputEventNumbers[0];
+	DerivedVars->zsp9 = currentEvent;
+	DerivedVars->zsp10 = syncLostOnThisEvent;
+	DerivedVars->zsp11 = syncCaughtOnThisEvent;
+	DerivedVars->zsp12 = Counters.primaryTeethSeen;
+	DerivedVars->zsp13 = Counters.secondaryTeethSeen;
+	DerivedVars->zsp14 = *ticksPerDegree;
+//	DerivedVars->zsp15 = Counters.testCounter4;
+//	DerivedVars->zsp16 = Counters.testCounter5;
+//	DerivedVars->zsp17 = Counters.testCounter6;
+	DerivedVars->zsp15 = Counters.camSyncLosses;
+	DerivedVars->zsp16 = Counters.crankSyncLosses;
+	DerivedVars->zsp17 = Counters.combustionSyncLosses;
+	DerivedVars->zsp18 = Counters.camSyncCorrections;
+	DerivedVars->zsp19 = syncLostWithThisID;
+//	DerivedVars->zsp18 = Counters.DwellStretchedToSchedule;
+//	DerivedVars->zsp19 = Counters.TooFarToSchedule;
+
+	/* UART/serial specific counters */
+//	DerivedVars->zsp? = Counters.serialEscapePairMismatches + Counters.serialStartsInsideAPacket + Counters.serialPacketsOverLength + Counters.serialNoiseErrors + Counters.serialOverrunErrors + Counters.serialFramingErrors + Counters.serialParityErrors;
+	/* Generic com counters */
+//	DerivedVars->zsp? = Counters.commsChecksumMismatches + Counters.commsPacketsUnderMinLength + Counters.commsDebugMessagesNotSent + Counters.commsErrorMessagesNotSent;
+
+}
