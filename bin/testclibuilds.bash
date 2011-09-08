e46c4b433a4865f87f6ddfdfdd7191d7d351239f
Fred Cooke <fred.cooke@gmail.com>
2011-09-09 03:58:30 +1200

    Add default build and volvo builds to test script.

--- bin/testclibuilds.bash
+++ bin/testclibuilds.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CLIFLAGS="-D CHECKDEFAULT=1" make clean s19
+sleep 1
 CLIFLAGS="-D XGATE=1" make clean  s19
 sleep 1
 CLIFLAGS="-D TRUCK=1" make clean  s19
@@ -11,5 +13,9 @@ sleep 1
 CLIFLAGS="-D SEANKR1=1" make clean  s19
 sleep 1
 CLIFLAGS="-D SEANKLT1=1" make clean  s19
+sleep 1
+CLIFLAGS="-D JOSHBROWN=1" make clean s19
+sleep 1
+CLIFLAGS="-D SNOTROCKET=1" make clean s19
 
 # Someone with elite bash skills grab the result from each of these and print them at the end, please.
