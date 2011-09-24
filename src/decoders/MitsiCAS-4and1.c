e234c2439dcd7024cada938cca6da9f817c5a7fe
Fred Cooke <fred.cooke@gmail.com>
2011-09-24 20:49:39 +0200

    Remove duplicated code from ages ago, replace with same code using macro from decoder interface.

--- src/decoders/MitsiCAS-4and1.c
+++ src/decoders/MitsiCAS-4and1.c
@@ -482,7 +482,9 @@ void PrimaryRPMISR(){
 		}
 	}
 
-	SCHEDULE_ECT_OUTPUTS();
+	if(decoderFlags & CAM_SYNC){
+		SCHEDULE_ECT_OUTPUTS();
+	}
 
 	if(decoderFlags & LAST_TIMESTAMP_VALID){
 		lastTicksPerDegree = thisTicksPerDegree;
@@ -599,26 +601,8 @@ void SecondaryRPMISR(){
 		}
 	}
 
-	// TODO behave differently depending upon sync level? Genericise this loop/logic? YES, move this to macro/function and call from all decoders.
 	if(decoderFlags & CAM_SYNC){
-		unsigned char outputEventNumber;
-		for(outputEventNumber=0;outputEventNumber<MAX_NUMBER_OF_OUTPUT_EVENTS;outputEventNumber++){
-			if(outputEventInputEventNumbers[outputEventNumber] == currentEvent){
-				skipEventFlags &= ~(1UL << outputEventNumber);
-				schedulePortTPin(outputEventNumber, timeStamp);
-			}else if(skipEventFlags & (1UL << outputEventNumber)){
-				unsigned char eventBeforeCurrent = 0;
-				if(currentEvent == 0){
-					eventBeforeCurrent = numberOfRealEvents - 1;
-				}else{
-					eventBeforeCurrent = currentEvent - 1;
-				}
-
-				if(outputEventInputEventNumbers[outputEventNumber] == eventBeforeCurrent){
-					schedulePortTPin(outputEventNumber, timeStamp);
-				}
-			}
-		}
+		SCHEDULE_ECT_OUTPUTS();
 	}
 
 	if(decoderFlags & LAST_TIMESTAMP_VALID){
