c67b10c7aa1f90e4e1baa6b348fe7a8ef831354a
Fred Cooke <fred.cooke@gmail.com>
2011-10-03 16:37:15 +0200

    Add serial comms stats to default logging in place of 3rd sched info which isn't much use anyway. DDRPM means that something was wrong with the contents or size of a packet, eg checksum. DTPS means the firmware didn't keep up with the incoming data stream and dropped a byte. DMAP means that your serial hardware is junk, get something better. djandruczyk (and others), keep an eye on these for your own interest :-)

--- src/default/datalogPopulator.c
+++ src/default/datalogPopulator.c
@@ -3,20 +3,22 @@
 void populateCustomDatalog(){
 //	CoreVars->DRPM = Counters.; currently in use logging *ticksPerDegree as at wed nzst 3:10pm 16 march 2011 see line ~98 of derivedVarsGenerator.c and line ~277 of coreVarsGenerator.c
 
-	// Currently unused, use as you wish:
-	CoreVars->DDRPM = injectorMainPulseWidthsMath[2];
-	CoreVars->DTPS = postReferenceEventDelays[2];
-	CoreVars->DMAP = outputEventInputEventNumbers[2];
+	// Comms issues:
+	CoreVars->DDRPM = Counters.commsChecksumMismatches + Counters.serialEscapePairMismatches + Counters.serialStartsInsideAPacket + Counters.commsPacketsUnderMinLength + Counters.serialPacketsOverLength; // Something was wrong with the contents or size of a packet
+	CoreVars->DTPS = Counters.serialOverrunErrors; // Interrupt load is too high, incoming packets end up with a byte or two missing
+	CoreVars->DMAP = Counters.serialNoiseErrors + Counters.serialFramingErrors + Counters.serialParityErrors; // Users hardware is junk
 
-	// Use these any way you like:
+	// Accurate clocks:
 	DerivedVars->zsp1 = Clocks.realTimeClockMain;
 	DerivedVars->zsp2 = Clocks.realTimeClockMillis;
+	// First two schedules contents:
 	DerivedVars->zsp3 = injectorMainPulseWidthsMath[0];
 	DerivedVars->zsp4 = postReferenceEventDelays[0];
 	DerivedVars->zsp5 = outputEventInputEventNumbers[0];
 	DerivedVars->zsp6 = injectorMainPulseWidthsMath[1];
 	DerivedVars->zsp7 = postReferenceEventDelays[1];
 	DerivedVars->zsp8 = outputEventInputEventNumbers[1];
+	// Decoder stuff:
 	DerivedVars->zsp9 = currentEvent;
 	DerivedVars->zsp10 = syncLostWithThisID;
 	DerivedVars->zsp11 = syncLostOnThisEvent;
@@ -26,12 +28,8 @@ void populateCustomDatalog(){
 	DerivedVars->zsp15 = Counters.decoderSyncLosses;
 	DerivedVars->zsp16 = Counters.decoderSyncCorrections;
 	DerivedVars->zsp17 = Counters.calculationsPerformed;
+
 //	DerivedVars->zsp? = Counters.tooFarToSchedule; currently unused in sched code
 //	DerivedVars->zsp? = Counters.dwellStretchedToSchedule; currently unused in sched code
-
-	/* UART/serial specific counters */
-//	DerivedVars->zsp? = Counters.serialEscapePairMismatches + Counters.serialStartsInsideAPacket + Counters.serialPacketsOverLength + Counters.serialNoiseErrors + Counters.serialOverrunErrors + Counters.serialFramingErrors + Counters.serialParityErrors;
-	/* Generic com counters */
-//	DerivedVars->zsp? = Counters.commsChecksumMismatches + Counters.commsPacketsUnderMinLength + Counters.commsDebugMessagesNotSent + Counters.commsErrorMessagesNotSent;
 
 }
