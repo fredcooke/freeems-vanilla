e4a8f6fbc3e7be788615c00311c39dc77a9e28fd
Fred Cooke <fred.cooke@gmail.com>
2011-10-12 16:57:53 +0200

    Fix for prelude :-)

--- src/inc/locationIDs.h
+++ src/inc/locationIDs.h
@@ -123,7 +123,7 @@
 #endif
 
 #define loggingSettingsLocationID                      0x9000
-#define loggingSettings2LocationID                     0x9000
+#define loggingSettings2LocationID                     0x9001
 
 #ifdef BLOCK_DETAILS_LOOKUP_C // only for here, external stuff should use the flags field for determining what to do/not do.
 #define SmallTableConfigs_FixedConfigBlocks_Border     0xA000
