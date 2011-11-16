73d3aa1b5598136acb6ce2414188ee7d27e93b8e
Fred Cooke <fred.cooke@gmail.com>
2011-11-16 18:29:45 +0100

    Change default logging to be useful and not empty.

--- src/default/datalogPopulator.c
+++ src/default/datalogPopulator.c
@@ -3,11 +3,24 @@
 void populateCustomDatalog(){
 //	CoreVars->DRPM = Counters.; currently in use logging *ticksPerDegree as at wed nzst 3:10pm 16 march 2011 see line ~98 of derivedVarsGenerator.c and line ~277 of coreVarsGenerator.c
 
-	// Currently spare:
+//	// Currently spare:
 //	CoreVars->DDRPM = injectorMainPulseWidthsMath[2];
 //	CoreVars->DTPS = postReferenceEventDelays[2];
 //	CoreVars->DMAP = outputEventInputEventNumbers[2];
-//
+
+	// Log scheduling data by default for the time being.
+	CoreVars->DTPS = Counters.normalSchedule;
+	CoreVars->DMAP = Counters.timerStretchedToSchedule;
+
+	KeyUserDebugs.zsp3 = Counters.pinScheduledToGoHigh;
+	KeyUserDebugs.zsp4 = Counters.pinScheduledAlready;
+	KeyUserDebugs.zsp5 = Counters.pinScheduledToSelfSchedule;
+	KeyUserDebugs.zsp6 = Counters.pinScheduledAgainToStayOn;
+	KeyUserDebugs.zsp7 = Counters.pinScheduledToToggleError;
+	KeyUserDebugs.zsp8 = Counters.pinScheduledToDoNothing;
+	KeyUserDebugs.zsp9 = Counters.pinScheduledFromCold;
+	KeyUserDebugs.zsp10 = Counters.pinScheduledWithTimerExtension;
+
 //	// First two schedules contents:
 //	KeyUserDebugs.zsp3 = injectorMainPulseWidthsMath[0];
 //	KeyUserDebugs.zsp4 = postReferenceEventDelays[0];
@@ -15,6 +28,6 @@ void populateCustomDatalog(){
 //	KeyUserDebugs.zsp6 = injectorMainPulseWidthsMath[1];
 //	KeyUserDebugs.zsp7 = postReferenceEventDelays[1];
 //	KeyUserDebugs.zsp8 = outputEventInputEventNumbers[1];
-//	KeyUserDebugs.zsp9 = ;
-//	KeyUserDebugs.zsp10 = ;
+//	KeyUserDebugs.zsp9 = 0;
+//	KeyUserDebugs.zsp10 = 0;
 }
