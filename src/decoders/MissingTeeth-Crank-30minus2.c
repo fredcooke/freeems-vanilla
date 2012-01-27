daf92c64d2ac6b6cb4622690310e168a30f1088d
Fred Cooke <fred.cooke@gmail.com>
2012-01-27 22:33:36 +0100

    Add 30-2 mode for testing purposes.

---
+++ src/decoders/MissingTeeth-Crank-30minus2.c
@@ -0,0 +1,45 @@
+/*	FreeEMS - the open source engine management system
+
+	Copyright 2011 Fred Cooke
+
+	This file is part of the FreeEMS project.
+
+	FreeEMS software is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	FreeEMS software is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with any FreeEMS software.  If not, see http://www.gnu.org/licenses/
+
+	We ask that if you make any changes to this file you email them upstream to
+	us at admin(at)diyefi(dot)org or, even better, fork the code on github.com!
+
+	Thank you for choosing FreeEMS to run your engine! */
+
+
+/** @file
+ *
+ * @ingroup interruptHandlers
+ * @ingroup enginePositionRPMDecoders
+ *
+ * @brief Missing teeth, 30-2, Crank only
+ *
+ * This just defines the required flags and values and includes the base header and base code.
+ *
+ * @note The decoderInterface header is included from the MissingTeeth header.
+ */
+
+
+#define DECODER_IMPLEMENTATION_C
+#define CRANK_ONLY
+#define TOTAL_TEETH   30
+#define MISSING_TEETH  2
+
+#include "inc/MissingTeeth.h"
+#include "code/MissingTeeth-Either-XminusY.c"
