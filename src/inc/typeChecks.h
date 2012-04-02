d320f51deec0811f2e6589ac0e90e9f90dac10d8
Fred Cooke <fred.cooke@gmail.com>
2012-04-02 20:33:03 +0200

    Add type checks for all other significant macros. Exclusions for shortcuts and stuff likely to change. Closes issue #549 and adds some robustness to the firmware.

--- src/inc/typeChecks.h
+++ src/inc/typeChecks.h
@@ -102,9 +102,68 @@ CASSERT(sizeof(DerivedVar) == 26, DerivedVar)
 CASSERT(sizeof(KeyUserDebug) == 40, KeyUserDebug)
 
 // Check the fixed point readability macros for correct behaviour
-CASSERT(KPA(655.3450000000) == KPA(655.35), PRESSURE) // Lowest to round up
-CASSERT(KPA(655.3549999999) == KPA(655.35), PRESSURE) // Highest to round down
-CASSERT(KPA(655.36) > SHORTMAX, PRESSURE)             // Overflow gets caught
+CASSERT(KPA(655.36) > SHORTMAX, PRESSURE)          // Overflow gets caught
+CASSERT(KPA(655.35) == SHORTMAX, PRESSURE)         // Centre == max
+CASSERT(KPA(655.3450000000) == SHORTMAX, PRESSURE) // Lowest to round up
+CASSERT(KPA(655.3549999999) == SHORTMAX, PRESSURE) // Highest to round down
+
+CASSERT(DEGREES_K(655.36) > SHORTMAX, TEMPERATURE_K)          // Overflow gets caught
+CASSERT(DEGREES_K(655.35) == SHORTMAX, TEMPERATURE_K)         // Centre == max
+CASSERT(DEGREES_K(655.3450000000) == SHORTMAX, TEMPERATURE_K) // Lowest to round up
+CASSERT(DEGREES_K(655.3549999999) == SHORTMAX, TEMPERATURE_K) // Highest to round down
+
+CASSERT(CC_PER_MINUTE(3840) > SHORTMAX, INJECTOR_FLOW)             // Overflow gets caught
+CASSERT(CC_PER_MINUTE(3839.94140625) == SHORTMAX, INJECTOR_FLOW)   // Centre == max
+CASSERT(CC_PER_MINUTE(3839.9701171875) == SHORTMAX, INJECTOR_FLOW) // Lowest to round up
+CASSERT(CC_PER_MINUTE(3839.9121093750) == SHORTMAX, INJECTOR_FLOW) // Highest to round down
+
+CASSERT(CYLINDER_VOLUME(2000) > SHORTMAX, CYL_VOLUME)            // Overflow gets caught
+CASSERT(CYLINDER_VOLUME(1999.969482422) == SHORTMAX, CYL_VOLUME) // Centre == max
+CASSERT(CYLINDER_VOLUME(1999.954223633) == SHORTMAX, CYL_VOLUME) // Lowest to round up
+CASSERT(CYLINDER_VOLUME(1999.984740906) == SHORTMAX, CYL_VOLUME) // Highest to round down
+
+CASSERT(VOLTS(65.536) > SHORTMAX, VOLTAGE)         // Overflow gets caught
+CASSERT(VOLTS(65.535) == SHORTMAX, VOLTAGE)        // Centre == max
+CASSERT(VOLTS(65.5345000001) == SHORTMAX, VOLTAGE) // Lowest to round up
+CASSERT(VOLTS(65.5354999999) == SHORTMAX, VOLTAGE) // Highest to round down
+
+CASSERT(LAMBDA(2.0) > SHORTMAX, LAMBDA)          // Overflow gets caught
+CASSERT(LAMBDA(1.999969482) == SHORTMAX, LAMBDA) // Centre == max
+CASSERT(LAMBDA(1.999954224) == SHORTMAX, LAMBDA) // Lowest to round up
+CASSERT(LAMBDA(1.999984741) == SHORTMAX, LAMBDA) // Highest to round down
+
+CASSERT(RPM(32767.75) > SHORTMAX, RPM)           // Overflow gets caught
+CASSERT(RPM(32767.5) == SHORTMAX, RPM)           // Centre == max
+CASSERT(RPM(32767.25) == SHORTMAX, RPM)          // Lowest to round up
+CASSERT(RPM(32767.74999999999) == SHORTMAX, RPM) // Highest to round down
+
+// Only used with values less than 720, overflows over 720 handled in the code
+CASSERT(ANGLE(1310.71) > SHORTMAX, ANGLE)          // Overflow gets caught
+CASSERT(ANGLE(1310.7) == SHORTMAX, ANGLE)          // Centre == max
+CASSERT(ANGLE(1310.69) == SHORTMAX, ANGLE)         // Lowest to round up
+CASSERT(ANGLE(1310.7099999999) == SHORTMAX, ANGLE) // Highest to round down
+
+CASSERT(PW_MS(52.4284) > SHORTMAX, PW_MS)          // Overflow gets caught
+CASSERT(PW_MS(52.428) == SHORTMAX, PW_MS)          // Centre == max
+CASSERT(PW_MS(52.4276) == SHORTMAX, PW_MS)         // Lowest to round up
+CASSERT(PW_MS(52.428399999999) == SHORTMAX, PW_MS) // Highest to round down
+
+CASSERT(PERCENT(102.39921875) > SHORTMAX, PERCENT)  // Overflow gets caught
+CASSERT(PERCENT(102.3984375) == SHORTMAX, PERCENT)  // Centre == max
+CASSERT(PERCENT(102.39765625) == SHORTMAX, PERCENT) // Lowest to round up
+CASSERT(PERCENT(102.39921874) == SHORTMAX, PERCENT) // Highest to round down
+
+CASSERT(VE(127.9990234375) > SHORTMAX, VE)     // Overflow gets caught
+CASSERT(VE(127.998046875) == SHORTMAX, VE)     // Centre == max
+CASSERT(VE(127.9970703125) == SHORTMAX, VE)    // Lowest to round up
+CASSERT(VE(127.9990234374999) == SHORTMAX, VE) // Highest to round down
+
+CASSERT(W(399.996948242188) > SHORTMAX, WARMUP)  // Overflow gets caught
+CASSERT(W(399.993896484375) == SHORTMAX, WARMUP) // Centre == max
+CASSERT(W(399.990844726563) == SHORTMAX, WARMUP) // Lowest to round up
+CASSERT(W(399.996948242187) == SHORTMAX, WARMUP) // Highest to round down
+
+// TODO IT() macro once new format is setup
 
 
 #else
