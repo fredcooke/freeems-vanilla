a16673e069a6e701e3c22b69c26e9e8a384242b6
Fred Cooke <fred.cooke@gmail.com>
2012-06-21 17:59:50 +0200

    Whitespace, rm author tag, update C year.

--- src/inc/errorDefines.h
+++ src/inc/errorDefines.h
@@ -1,6 +1,6 @@
 /* FreeEMS - the open source engine management system
  *
- * Copyright 2008-2011 Fred Cooke
+ * Copyright 2008-2012 Fred Cooke
  *
  * This file is part of the FreeEMS project.
  *
@@ -34,8 +34,6 @@
  * This file contains all error codes for propagation up and out to a PC.
  * When adding new codes, ensure that they are unique and also that you
  * group them appropriately in ranges for easy visual identification.
- *
- * @author Fred Cooke
  */
 
 
@@ -48,89 +46,89 @@
 /** @todo TODO Introduce some structure to the numbering such that they can be interpreted somewhat without a chart */
 
 // A OK Nothing is wrong No error error.
-#define NO_PROBLEMO						0x0666
+#define NO_PROBLEMO                     0x0666
 
 /* Unconfigured options */
-#define IAT_NOT_CONFIGURED_CODE			0x1000
-#define CHT_NOT_CONFIGURED_CODE			0x1001
-#define TPS_NOT_CONFIGURED_CODE			0x1002
-#define EGO_NOT_CONFIGURED_CODE			0x1003
-#define BRV_NOT_CONFIGURED_CODE			0x1004
-#define MAP_NOT_CONFIGURED_CODE			0x1005
-#define AAP_NOT_CONFIGURED_CODE			0x1006
-#define MAT_NOT_CONFIGURED_CODE			0x1007
-#define EGO2_NOT_CONFIGURED_CODE		0x1008
-#define IAP_NOT_CONFIGURED_CODE			0x1009
-#define LOAD_NOT_CONFIGURED_CODE		0x100A
-#define AIRFLOW_NOT_CONFIGURED_CODE		0x100B
-#define BPW_NOT_CONFIGURED_CODE			0x100C
+#define IAT_NOT_CONFIGURED_CODE         0x1000
+#define CHT_NOT_CONFIGURED_CODE         0x1001
+#define TPS_NOT_CONFIGURED_CODE         0x1002
+#define EGO_NOT_CONFIGURED_CODE         0x1003
+#define BRV_NOT_CONFIGURED_CODE         0x1004
+#define MAP_NOT_CONFIGURED_CODE         0x1005
+#define AAP_NOT_CONFIGURED_CODE         0x1006
+#define MAT_NOT_CONFIGURED_CODE         0x1007
+#define EGO2_NOT_CONFIGURED_CODE        0x1008
+#define IAP_NOT_CONFIGURED_CODE         0x1009
+#define LOAD_NOT_CONFIGURED_CODE        0x100A
+#define AIRFLOW_NOT_CONFIGURED_CODE     0x100B
+#define BPW_NOT_CONFIGURED_CODE         0x100C
 
 
 /* Badly configured options */
-#define VE_TABLE_MAIN_LOAD_LENGTH_TOO_LONG	0x2000
-#define VE_TABLE_MAIN_RPM_LENGTH_TOO_LONG	0x2001
-#define VE_TABLE_MAIN_MAIN_LENGTH_TOO_LONG	0x2002
-#define BRV_MAX_TOO_LARGE					0x2003
+#define VE_TABLE_MAIN_LOAD_LENGTH_TOO_LONG 0x2000
+#define VE_TABLE_MAIN_RPM_LENGTH_TOO_LONG  0x2001
+#define VE_TABLE_MAIN_MAIN_LENGTH_TOO_LONG 0x2002
+#define BRV_MAX_TOO_LARGE                  0x2003
 
 
 /* Flash burning error codes */
-#define sizeNotMultipleOfSectorSize		0x3000
-#define sizeOfBlockToBurnIsZero			0x3001
-#define smallBlockCrossesSectorBoundary	0x3002
-#define addressNotSectorAligned			0x3003
-#define addressNotWordAligned			0x3004
-#define addressNotFlashRegion			0x3005
-#define flashEraseFailed				0x3006
-#define flashAccessError				0x3007
-#define flashProtectionError			0x3008
+#define sizeNotMultipleOfSectorSize     0x3000
+#define sizeOfBlockToBurnIsZero         0x3001
+#define smallBlockCrossesSectorBoundary 0x3002
+#define addressNotSectorAligned         0x3003
+#define addressNotWordAligned           0x3004
+#define addressNotFlashRegion           0x3005
+#define flashEraseFailed                0x3006
+#define flashAccessError                0x3007
+#define flashProtectionError            0x3008
 
 
 // memory check error
-#define MEMORY_WRITE_ERROR                      0x3009
+#define MEMORY_WRITE_ERROR                   0x3009
 
 /* Communications error codes */
-#define unimplementedFunction                   0x4000
-#define packetChecksumMismatch                  0x4001
-#define packetTooShortForSpecifiedFields        0x4002
-#define doesNotMakeSenseToRetrievePartially     0x4003
-#define payloadLengthTypeMismatch               0x4004
-#define payloadLengthHeaderMismatch             0x4005
-#define invalidPayloadID                        0x4006
-#define unrecognisedPayloadID                   0x4007
-#define invalidMemoryActionForID                0x4008
-#define invalidIDForMainTableAction             0x4009
-#define invalidIDForTwoDTableAction             0x400A
-#define noSuchAsyncDatalogType                  0x400B
-#define datalogLengthExceedsMax                 0x400C
-#define locationIDNotFound                      0x400D
-#define requestedRAMPageInvalid                 0x400E
-#define requestedFlashPageInvalid               0x400F
-#define requestedLengthTooLarge                 0x4010
-#define requestedAddressDisallowed              0x4011
-#define invalidSizeOffsetCombination            0x4012
-#define uncheckedTableManipulationNotAllowed    0x4013
-#define payloadNotEqualToSpecifiedValue         0x4014
-#define noSuchLocationIDListType                0x4015
-#define payloadShorterThanRequiredForTest       0x4016
-#define noSuchUnitTestID                        0x4017
-#define attemptToWriteToReadOnlyBlock           0x4018
+#define unimplementedFunction                0x4000
+#define packetChecksumMismatch               0x4001
+#define packetTooShortForSpecifiedFields     0x4002
+#define doesNotMakeSenseToRetrievePartially  0x4003
+#define payloadLengthTypeMismatch            0x4004
+#define payloadLengthHeaderMismatch          0x4005
+#define invalidPayloadID                     0x4006
+#define unrecognisedPayloadID                0x4007
+#define invalidMemoryActionForID             0x4008
+#define invalidIDForMainTableAction          0x4009
+#define invalidIDForTwoDTableAction          0x400A
+#define noSuchAsyncDatalogType               0x400B
+#define datalogLengthExceedsMax              0x400C
+#define locationIDNotFound                   0x400D
+#define requestedRAMPageInvalid              0x400E
+#define requestedFlashPageInvalid            0x400F
+#define requestedLengthTooLarge              0x4010
+#define requestedAddressDisallowed           0x4011
+#define invalidSizeOffsetCombination         0x4012
+#define uncheckedTableManipulationNotAllowed 0x4013
+#define payloadNotEqualToSpecifiedValue      0x4014
+#define noSuchLocationIDListType             0x4015
+#define payloadShorterThanRequiredForTest    0x4016
+#define noSuchUnitTestID                     0x4017
+#define attemptToWriteToReadOnlyBlock        0x4018
 
-#define invalidAxisOrder				1 /* prevent parsing */
-#define invalidAxisIndex				2 /* prevent parsing */
+#define invalidAxisOrder 1 /* prevent parsing */
+#define invalidAxisIndex 2 /* prevent parsing */
 
-#define errorBaseMainTableRPM			0x6000
-#define invalidMainTableRPMOrder		0x6001
-#define invalidMainTableRPMIndex		0x6002
-#define invalidMainTableRPMLength		0x6003
-#define errorBaseMainTableLoad			0x6004
-#define invalidMainTableLoadOrder		0x6005
-#define invalidMainTableLoadIndex		0x6006
-#define invalidMainTableLoadLength		0x6007
-#define invalidMainTableMainLength		0x6008
+#define errorBaseMainTableRPM           0x6000
+#define invalidMainTableRPMOrder        0x6001
+#define invalidMainTableRPMIndex        0x6002
+#define invalidMainTableRPMLength       0x6003
+#define errorBaseMainTableLoad          0x6004
+#define invalidMainTableLoadOrder       0x6005
+#define invalidMainTableLoadIndex       0x6006
+#define invalidMainTableLoadLength      0x6007
+#define invalidMainTableMainLength      0x6008
 
-#define errorBaseTwoDTableAxis			0x6010
-#define invalidTwoDTableAxisOrder		0x6011
-#define invalidTwoDTableIndex			0x6012
+#define errorBaseTwoDTableAxis          0x6010
+#define invalidTwoDTableAxisOrder       0x6011
+#define invalidTwoDTableIndex           0x6012
 
 #define thisIsNotTheBenchTestDecoder    0x7777
 #define tooShortOfAPulseWidthToTest     0x7778
