0ac8094057d44eeb83e0297da0c63cd25164a858
Fred Cooke <fred.cooke@gmail.com>
2012-07-09 17:38:19 +0200

    Upgrade cli test script MASSIVELY to build in a loop and collect stats and statuses and do a report at the end.

--- bin/testclibuilds.bash
+++ bin/testclibuilds.bash
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-CLIFLAGS="-D DEFAULT=1" make clean s19
-sleep 1
-CLIFLAGS="-D XGATE=1" make clean s19
-sleep 1
-CLIFLAGS="-D TRUCK=1" make clean s19
-sleep 1
-CLIFLAGS="-D HOTEL=1" make clean s19
-sleep 1
-CLIFLAGS="-D PRESTO=1" make clean s19
-sleep 1
-CLIFLAGS="-D SEANKR1=1" make clean s19
-sleep 1
-CLIFLAGS="-D SEANKLT1=1" make clean s19
-sleep 1
-CLIFLAGS="-D JOSHBROWN=1" make clean s19
-sleep 1
-CLIFLAGS="-D SNOTROCKET=1" make clean s19
-sleep 1
-CLIFLAGS="-D PETERJSERIES=1" make clean s19
-sleep 1
-CLIFLAGS="-D SLATER=1" make clean s19
-sleep 1
-CLIFLAGS="-D SPUDMN=1" make clean s19
+builds=('DEFAULT' 'XGATE' 'TRUCK' 'HOTEL' 'PRESTO' 'SEANKLT1' 'SNOTROCKET' 'SPUDMN' 'SLATER' 'PETERJSERIES' 'DEUCECOUPE' 'PETERTRUCK')
 
-# Someone with elite bash skills grab the result from each of these and print them at the end, please.
+results[${#builds[*]} - 1]=0
+failures=0
+
+# Perform the tests
+for ((i=0; i<${#builds[*]}; i++))
+do
+  CLIFLAGS="-D ${builds[i]}=1" make clean s19
+  results[i]=$?
+  if ((${results[i]} != 0)); then
+    failures=$(($failures+1))
+  fi
+done
+
+# Report it all
+echo
+echo "#################" Built the firmware ${#builds[*]} times with $failures failures! "#################"
+echo Passes:
+for ((i=0; i<${#builds[*]}; i++))
+do
+  if ((${results[$i]} == 0)); then
+    echo ${builds[i]} passed!
+  fi
+done
+if (($failures != 0)); then
+  echo
+  echo Failures:
+  for ((i=0; i<${#builds[*]}; i++))
+  do
+    if ((${results[$i]} != 0)); then
+      echo ${builds[i]} FAILED with result ${results[$i]}
+    fi
+  done
+fi
+echo "################################################################################"
