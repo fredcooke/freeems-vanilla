83ad5ffb856565d6a951f686b0e596654b8dd16d
Fred Cooke <fred.cooke@gmail.com>
2014-04-11 22:56:07 +1200

    Fix some directory documentation that had become out of date.

--- src/main/doxy/directories.h
+++ src/main/doxy/directories.h
@@ -1,6 +1,6 @@
 /* FreeEMS - the open source engine management system
  *
- * Copyright 2012 Fred Cooke
+ * Copyright 2014 Fred Cooke
  *
  * This file is part of the FreeEMS project.
  *
@@ -31,7 +31,7 @@
  */
 
 
-/** @dir src
+/** @dir src/main
  *
  * The home of all source files used to build FreeEMS firmware. Groups of related
  * files are stored in sub-directories within this directory and general files
@@ -39,15 +39,15 @@
  */
 
 
-/** @dir src/assembly
+/** @dir src/main/assemblyhand
  *
- * The very small amount of S12X assembly used in the project lives here, as do
- * all assembly files generated during the build process. Cleaning is done using
- * specific file names so as to not remove the hand written files.
+ * The very small amount of hand-written S12X assembly used in the project lives
+ * here. All assembly files generated during the build process live in a temporary
+ * directory called assembly.
  */
 
 
-/** @dir src/assembly/xgate
+/** @dir src/main/assemblyxgate
  *
  * Assembly source files for the XGATE coprocessor are stored in this directory.
  * All XGATE assembly source files are hand written as it does not make sense to
@@ -55,7 +55,7 @@
  */
 
 
-/** @dir src/data
+/** @dir src/main/data
  *
  * This directory contains no actual code, just data used to initialise various
  * blocks of memory such as structs and arrays etc. These files get pulled into
@@ -63,37 +63,37 @@
  */
 
 
-/** @dir src/data/tables
+/** @dir src/main/data/tables
  *
  * This directory contains the various data components that make up tables.
  */
 
 
-/** @dir src/data/tables/axis
+/** @dir src/main/data/tables/axis
  *
  * This directory has the axis data for the RPM and Load axes in it.
  */
 
 
-/** @dir src/data/tables/ign
+/** @dir src/main/data/tables/ign
  *
  * This directory has the ignition timing table data in it.
  */
 
 
-/** @dir src/data/tables/lambda
+/** @dir src/main/data/tables/lambda
  *
  * This directory has the integral lambda table data in it.
  */
 
 
-/** @dir src/data/tables/ve
+/** @dir src/main/data/tables/ve
  *
  * This directory has the volumetric efficiency table data in it.
  */
 
 
-/** @dir src/data/thermistors
+/** @dir src/main/data/thermistors
  *
  * This directory contains a library of data files for common combinations of
  * bias resistor and thermistors for three main types of sensor and six commonly
@@ -101,7 +101,7 @@
  */
 
 
-/** @dir src/decoders
+/** @dir src/main/decoders
  *
  * The RPM/position decoder directory contains ISR code for various types of RPM
  * and position sensor combinations. Buildable files can be found at this level.
@@ -109,40 +109,32 @@
  */
 
 
-/** @dir src/decoders/code
+/** @dir src/main/decoders/code
  *
  * Code that is re-used across multiple build configurations lives here.
  */
 
 
-/** @dir src/decoders/inc
+/** @dir src/main/decoders/inc
  *
  * Header files for decoder ISRs are stored here, whether one-off or re-used.
  */
 
 
-/** @dir src/default
- *
- * When files are often customised, they exist in pairs, the defaults goes here,
- * and the user versions go in src/custom which doxygen doesn't list.
- */
-
-
-/** @dir src/doxy
+/** @dir src/main/doxy
  *
  * Files such as the source of this directory documentation which are purely
  * present for Doxygen to parse and display are stored here.
  */
 
 
-/** @dir src/inc
+/** @dir src/main/inc
  *
  * Normal header files of global or single file use nature live here.
  */
 
 
-/** @dir src/initialisers
+/** @dir src/main/initialisers
  *
  * Files that are purely data initialisers and contain no logic code live here.
  */
-
