ea11188929e8dfe08cc77c65c5931ddd3c5384de
m2cupcar <rob@ebersol.com>
2014-08-22 12:38:40 -0400

    Config updates
    Need to figure out decoder offset

--- src/main/FreeEMS/FixedConfig1.c
+++ src/main/FreeEMS/FixedConfig1.c
@@ -75,7 +75,7 @@ const volatile fixedConfig1 fixedConfigs1 FIXEDCONF1 = {
 		injectorFlow:       CC_PER_MINUTE(235),
 #elif CONFIG == M2CUPCAR_ID
 		perCylinderVolume:  CYLINDER_VOLUME(500),
-		injectorFlow:       CC_PER_MINUTE(550), // RX7 "550" flowed at 580cc
+		injectorFlow:       CC_PER_MINUTE(550), // RX7 550CC SAT
 #elif CONFIG == SCAVENGER_ID
 		perCylinderVolume:  CYLINDER_VOLUME(399.25),
 		injectorFlow:       CC_PER_MINUTE(540),
@@ -194,11 +194,11 @@ const volatile fixedConfig1 fixedConfigs1 FIXEDCONF1 = {
 		numberOfConfiguredOutputEvents:          6, // THESE ARE IGN, THEY ARE NOT FUEL
 		numberOfInjectionsPerEngineCycle:        1  // Sequential, baby, yeah!
 
-#elif CONFIG == M2CUPCAR_ID // M2cupcar Mazda FE3 MX5/Miata, stock sensors, CAS and Coils
+#elif CONFIG == M2CUPCAR_ID // M2cupcar Mazda FE-dohc MX5/Miata, stock sensors, CAS and Coils
 		anglesOfTDC: {ANGLE(0), ANGLE(180), ANGLE(360), ANGLE(540), ANGLE(0), ANGLE(180), ANGLE(360), ANGLE(540)},
 		outputEventPinNumbers:       {0,1,0,1,2,3,2,3}, // Wasted spark and semi-sequential
 		schedulingConfigurationBits: {0,0,0,0,1,1,1,1}, // First four ignition, last four fuel
-		decoderEngineOffset:              ANGLE(90.00), // FE-DOHC, 4and1 CAS approximately centre
+		decoderEngineOffset:              ANGLE(90.00), // FE-DOHC, 4and1 CAS- need to figure out offset
 		numberOfConfiguredOutputEvents:              8, // 
 		numberOfInjectionsPerEngineCycle:            2  // Semi-sequential
 
